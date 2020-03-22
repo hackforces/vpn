ff6cfab0c7d9f8dab2676a2d79bd2f6a1d7ccb9e
Stayer <rodionov12@gmail.com>
2020-03-22 15:57:38 +0300

    Push to github packages
    Signed-off-by: Stayer <rodionov12@gmail.com>

--- ci/pub.sh
+++ ci/pub.sh
@@ -1,2 +1,7 @@
+# Publishing to yandex cloud
 docker build -f ci/Dockerfile -t cr.yandex/crpnjr9i2f2ggh9opdjh/hf_vpn:latest .
 docker push cr.yandex/crpnjr9i2f2ggh9opdjh/hf_vpn:latest
+
+# Publishing to github
+docker tag cr.yandex/crpnjr9i2f2ggh9opdjh/hf_vpn:latest docker.pkg.github.com/hackforces/vpn/vpn:latest
+docker push docker.pkg.github.com/hackforces/vpn/vpn:latest
